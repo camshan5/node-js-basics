e59c1177a08be23b174c46b0fb8eb99fda5494a0
Cameron Shannon <cameroncshannon@gmail.com>
2020-02-06 13:45:07 -0800

    s3v1 - s3v2

--- profile-app/app.js
+++ profile-app/app.js
@@ -3,6 +3,11 @@
 
 const https = require("https");
 
+// Print Error Messages
+const printError = error => {
+  console.error(error.message);
+};
+
 // Function to print message to console
 const printMessage = (username, badgeCount, points) => {
   const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
@@ -10,31 +15,42 @@ const printMessage = (username, badgeCount, points) => {
 };
 
 const getProfile = username => {
-  // Connect to the API URL
-  const request = https.get(
-    `https://teamtreehouse.com/${username}.json`,
-    response => {
-      let body = "";
+  try {
+    // Connect to the API URL
+    const request = https.get(
+      `https://teamtreehouse.com/${username}.json`,
+      response => {
+        let body = "";
 
-      // Read the Data:
-      response.on("data", data => {
-        body += data.toString();
-      });
+        // Read the Data:
+        response.on("data", data => {
+          body += data.toString();
+        });
 
-      response.on("end", () => {
-        // Parse the Data:
-        const profile = JSON.parse(body);
+        response.on("end", () => {
+          try {
+            // Parse the Data:
+            const profile = JSON.parse(body);
 
-        // Print the Data:
-        printMessage(
-          username,
-          profile["badges"].length,
-          profile.points["JavaScript"]
-        );
-      });
-    }
-  );
+            // Print the Data:
+            printMessage(
+              username,
+              profile["badges"].length,
+              profile.points["JavaScript"]
+            );
+          } catch (error) {
+            printError(error);
+          }
+        });
+      }
+    );
+    request.on("error", printError);
+  } catch (error) {
+    printError(error);
+  }
 };
+
+// users = ["camshan5", "chalkers"]
 
 // Note: argv is an array property
 const users = process.argv.slice(2); // index 3 start
