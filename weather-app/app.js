02466e4db27ba4ce9b20ed1e51871fc86006451f
Cameron Shannon <cameroncshannon@gmail.com>
2020-02-07 16:08:44 -0800

    Updated with darksky api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,7 @@
-const weather = require('./weather');
-//Join multiple values passed as arguments and replace all spaces with underscores
-const query = process.argv.slice(2).join("_").replace(' ', '_');
-//query: 90201
-//query: Cleveland_OH
-//query: London_England
-weather.get(query);
+const weather = require("./weather");
+const query = process.argv.slice(2);
+
+lat = query[0];
+lon = query[1];
+
+weather.get(lat, lon);
